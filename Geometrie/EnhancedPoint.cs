2b96a4f3a5391e9ca9d58d4a0acb5f4c7f043973
Hans-Michael Odenthal <450547@fom-net.de>
2018-01-23 19:20:03 +0100

    Alles anders

--- Geometrie/EnhancedPoint.cs
+++ Geometrie/EnhancedPoint.cs
@@ -11,7 +11,7 @@ namespace Geometrie
         EnhancedPoint()
         {
             x = 0;
-            y = 0; 
+            y = 0;
         }
 
         EnhancedPoint(Point p)
@@ -30,6 +30,35 @@ namespace Geometrie
         {
             //string ret = "x-Wert:" + x.ToString() + ", y-Wert:" + y.ToString();
             return string.Format("x-Wert: {0}, y-Wert: {1}", x, y);
+        }
+
+        public static EnhancedPoint operator +(EnhancedPoint punkt1,EnhancedPoint punkt2)
+            {
+                return new EnhancedPoint (punkt1.x + punkt2.x, punkt1.y +punkt2.y)
+            }
+        public static EnhancedPoint operator -(EnhancedPoint punkt1, EnhancedPoint punkt2)
+        {
+            return new EnhancedPoint(punkt1.x - punkt2.x, punkt1.y - punkt2.y)   
+        }
+
+        public static bool operator ==(EnhancedPoint punkt1, EnhancedPoint punkt2)
+        {
+            return (punkt1.x == punkt2.x && punkt1.y == punkt2.y);
+        }
+
+        public static bool operator !=(EnhancedPoint punkt1, EnhancedPoint punkt2)
+        {
+            return !(punkt1 == punkt2);
+        }
+
+        public static double distance(EnhancedPoint punkt1, EnhancedPoint punkt2)
+        {
+            return Math.Sqrt(Math.Pow((punkt1.x - punkt2.x), 2) + Math.Pow((punkt1.y - punkt2.y), 2));
+        }
+
+        public static explicit operator float(EnhancedPoint punkt1)
+        {
+            return (float)Math.Sqrt(Math.Pow((punkt1.x), 2) + Math.Pow(punkt1.y, 2));
         }
     }
 }
