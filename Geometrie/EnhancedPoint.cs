8a648c4a2074304f0c1a4bcf919ae19b54ee53bc
Hans-Michael Odenthal <450547@fom-net.de>
2018-01-23 18:44:19 +0100

    Projektdateien hinzufügen.

---
+++ Geometrie/EnhancedPoint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometrie
+{
+    class EnhancedPoint : Point
+    {
+        EnhancedPoint()
+        {
+            x = 0;
+            y = 0; 
+        }
+
+        EnhancedPoint(Point p)
+        {
+            x = p.x;
+            y = p.y;
+        }
+
+        EnhancedPoint(int koordx, int koordy)
+        {
+            x = koordx;
+            y = koordy;
+        }
+
+        public override string toString()
+        {
+            //string ret = "x-Wert:" + x.ToString() + ", y-Wert:" + y.ToString();
+            return string.Format("x-Wert: {0}, y-Wert: {1}", x, y);
+        }
+    }
+}
---
+++ Geometrie/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometrie
+{
+    public class Point
+
+    {
+        private int _x, _y;
+        public int x
+        {
+            get { return _x; }
+            set { _x = value; }
+
+        }
+        public int y
+        {
+            get { return _y; }
+            set { _y = value; }
+
+        }
+
+    }
+}
